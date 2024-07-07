e3356b7b25468fd19d4620d97d46e9234f53c60f
David <davidvillegasr@gmail.com>
2024-07-06 22:40:25 -0400

    commit to see if everyting is working fine

--- src/main/java/webdevelopment/jakartaee_pojo_tp/service/TaxCalculator.java
+++ src/main/java/webdevelopment/jakartaee_pojo_tp/service/TaxCalculator.java
@@ -4,6 +4,6 @@ public class TaxCalculator {
 
     // psvm to test if the program works
     public static void main(String[] args) {
-        System.out.println("hello all");
+        System.out.println("por fin Carajo!");
     }
 }
